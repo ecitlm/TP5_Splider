79290f7f062ce81dd095fe108c60be99e1cba1cb
ecitlm@163.com <ecitlm@163.com>
2017-06-02 11:06:13 +0800

    edit application to apps

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ header("Access-Control-Allow-Origin: *");
 //入口文件绑定模块名
 //define('BIND_MODULE','api');
 
-define('APP_PATH', __DIR__ . '/application/');
+define('APP_PATH', __DIR__ . '/apps/');
 // 加载框架引导文件
 require __DIR__ . '/thinkphp/start.php';
--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
 
-define('APP_PATH', __DIR__ . '/../application/');
+define('APP_PATH', __DIR__ . '/../apps/');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
