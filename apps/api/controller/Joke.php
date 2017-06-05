456a174a3e5c25bbcfd45291df010760709341b3
ecitlm@163.com <ecitlm@163.com>
2017-06-05 17:20:24 +0800

    update

--- apps/api/controller/Joke.php
+++ apps/api/controller/Joke.php
@@ -12,7 +12,7 @@ class Joke
     public function index($page = 10)
     {
         $page = (isset($_GET['page'])) ? intval($_GET ['page']) : 10;
-        $url = "http://3g.163.com/touch/jsonp/joke/chanListNews/T141931628472/2/" . $page . "-10.html?callback=data";
+        $url = "http://3g.163.com/touch/jsonp/joke/chanListNews/T141931628472/2/{$page}-10.html?callback=data";
         $res = HttpGet($url);
         $arr = json_decode(substr($res, 5, -1), true);
         return json([
