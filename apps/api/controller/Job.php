d96a70905fb8adc2206970a64b2d09fae4c43ecd
ecitlm <ecitlm@163.com>
2017-09-21 22:29:25 +0800

    add detail

--- apps/api/controller/Job.php
+++ apps/api/controller/Job.php
@@ -7,17 +7,38 @@
  */
 
 namespace app\api\controller;
+include('QueryList.php');
 class Job{
     public function index(){
         $pageNo = (isset($_GET['pageNo'])) ? intval($_GET ['pageNo']) : 1;
-        $type= (isset($_GET['type'])) ? $_GET ['pageNo'] : "web";
-        $url = "https://m.lagou.com/search.json?city=%E6%B7%B1%E5%9C%B3&positionName={$type}&pageNo={$pageNo}&pageSize=3";
+        $url = "https://m.lagou.com/search.json?city=%E6%B7%B1%E5%9C%B3&positionName=web&pageNo={$pageNo}&pageSize=15";
         $res = HttpGet($url);
         return json([
             'msg' => 'success',
             'code' => 1,
             'domain'=>'https://static.lagou.com',
             'data' => json_decode($res, true)['content']['data']
+        ]);
+    }
+
+    public  function  dedtail(){
+        $positionId = (isset($_GET['positionId'])) ? $_GET ['positionId'] : "2662779";
+        $url="https://m.lagou.com/jobs/{$positionId}.html";
+        $res = HttpGet($url);
+        \phpQuery::newDocumentHTML($res);
+        $result=array(
+            'title'=>pq('.title')->text(),
+            "content"=>pq('.content')->html(),
+            'salary'=>pq('.salary')->find('.text')->html(),
+            'workyear'=>pq('.workyear')->find('.text')->text(),
+            'workaddress'=> pq('.workaddress')->find('.text')->text(),
+            'education'=> pq('.education')->find('.text')->text(),
+            'temptation'=> pq('.temptation')->text(),
+        );
+        return json([
+            'msg' => 'success',
+            'code' => 1,
+            'data' => $result
         ]);
     }
 }
