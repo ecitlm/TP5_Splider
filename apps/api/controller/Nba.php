9414087faa669901b0752eb6fe512443a2352cbb
ecitlm <ecitlm@163.com>
2017-10-27 18:52:26 +0800

    asd

--- apps/api/controller/Nba.php
+++ apps/api/controller/Nba.php
@@ -13,54 +13,83 @@ namespace app\api\controller;
 class Nba
 {
 
-	/**
-	 * [获取赛事直播]
-	 * @return [type] [description]
-	 */
-    public function  schedule(){
-    	$md = (isset($_GET['date'])) ? $_GET ['date'] : "";
-    	$res = HttpGet("https://nb.3g.qq.com/nba/api/schedule@getList?md={$md}&sid=");
-    	return json([
+    /**
+     * [获取赛事直播]
+     */
+    public function schedule()
+    {
+        $md = (isset($_GET['date'])) ? $_GET ['date'] : "";
+        $res = HttpGet("https://nb.3g.qq.com/nba/api/schedule@getList?md={$md}&sid=");
+        return json([
             'msg' => 'success',
             'code' => 1,
-            'data' => json_decode($res,true)['schedule@getList']
+            'data' => json_decode($res, true)['schedule@getList']
         ]);
-    
+
     }
 
 
-	/**
-	 * [live_detail 比赛直播详情信息]
-	 * @return [type] [description]
-	 */
-    public function live_detail(){
-
-    	$schid = (isset($_GET['schid'])) ? $_GET ['schid'] : "1470215";
-    	$liveid = (isset($_GET['liveid'])) ? $_GET ['liveid'] : "2009587";
-    	$res = HttpGet("https://nb.3g.qq.com/nba/api/live@getInfo?i_schid={$schid}&i_liveid={$liveid}");
-    	return json([
+    /**
+     * [live_detail 比赛直播详情信息]
+     */
+    public function live_detail()
+    {
+        $schid = (isset($_GET['schid'])) ? $_GET ['schid'] : "1470215";
+        $liveid = (isset($_GET['liveid'])) ? $_GET ['liveid'] : "2009587";
+        $res = HttpGet("https://nb.3g.qq.com/nba/api/live@getInfo?i_schid={$schid}&i_liveid={$liveid}");
+        return json([
             'msg' => 'success',
             'code' => 1,
-            'data' => json_decode($res,true)['live@getInfo']['data']
+            'data' => json_decode($res, true)['live@getInfo']['data']
         ]);
-
-    	
-
     }
+
+
+    /**
+     * 直播内容
+     * @return \think\response\Json
+     */
+    public function live_content()
+    {
+        $schid = (isset($_GET['schid'])) ? $_GET ['schid'] : "1470215";
+        $res = HttpGet("https://live.3g.qq.com/g/s?aid=action_api&module=nba&action=broadcast_content%2Cbroadcast_info&sch_id={$schid}");
+        return json([
+            'msg' => 'success',
+            'code' => 1,
+            'data' => json_decode($res, true)['broadcast_content']['contentAry']
+        ]);
+    }
+
+    /**
+     * 球员技术统计
+     * @return \think\response\Json
+     */
+    public function technical_statistics()
+    {
+
+        $schid = (isset($_GET['schid'])) ? $_GET ['schid'] : "1470215";
+        $res = HttpGet("https://live.3g.qq.com/g/s?aid=action_api&module=nba&action=live_stat_4_nba%2Cbroadcast_info&sch_id={$schid}");
+        return json([
+            'msg' => 'success',
+            'code' => 1,
+            'data' => json_decode($res, true)['live_stat_4_nba']['playStat']
+        ]);
+    }
+
     /**
      * 转发图片
-     * @return [type] [description]
      */
-    public function img(){
-    	$filename=(isset($_GET['imgurl'])) ? $_GET ['imgurl'] : "https://code.it919.cn/img/head.jpg";
-		$size = getimagesize($filename);
-		$fp = fopen($filename, "rb");
-		if ($size && $fp) {
-		    header("Content-type: {$size['mime']}");
-		    fpassthru($fp);
-		    exit;
-		} else {
-		   echo "<img src='https://code.it919.cn/img/head.jpg'>";
-		}
+    public function img()
+    {
+        $filename = (isset($_GET['imgurl'])) ? $_GET ['imgurl'] : "https://code.it919.cn/img/head.jpg";
+        $size = getimagesize($filename);
+        $fp = fopen($filename, "rb");
+        if ($size && $fp) {
+            header("Content-type: {$size['mime']}");
+            fpassthru($fp);
+            exit;
+        } else {
+            echo "<img src='https://code.it919.cn/img/head.jpg'>";
+        }
     }
 }
