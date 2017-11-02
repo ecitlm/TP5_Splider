c08c2aaba75ae13545425827db39c786ec9a7339
ecitlm <ecitlm@163.com>
2017-11-02 18:18:36 +0800

    add

--- apps/api/controller/Nba.php
+++ apps/api/controller/Nba.php
@@ -9,12 +9,11 @@
 
 namespace app\api\controller;
 
-
 class Nba
 {
-
     /**
-     * [获取赛事直播]
+     * @return \think\response\Json
+     *  获取赛事直播列表
      */
     public function schedule()
     {
@@ -46,8 +45,8 @@ class Nba
 
 
     /**
-     * 直播内容
      * @return \think\response\Json
+     * 直播内容
      */
     public function live_content()
     {
@@ -76,7 +75,12 @@ class Nba
         ]);
     }
 
-    public function  player_detail(){
+    /**
+     * @return \think\response\Json
+     * 球员详情
+     */
+    public function player_detail()
+    {
         $playerid = (isset($_GET['playerid'])) ? $_GET ['playerid'] : "4130";
         $res = HttpGet("https://live.3g.qq.com/g/s?aid=action_api&module=nba&action=player_detail&playerId={$playerid}&sid=");
         return json([
@@ -91,7 +95,8 @@ class Nba
      * @return \think\response\Json
      * 联盟排名
      */
-    public function team_rank(){
+    public function team_rank()
+    {
         $res = HttpGet("https://matchweb.sports.qq.com/rank/team?columnId=100000&from=NBA");
         return json([
             'msg' => 'success',
@@ -104,7 +109,8 @@ class Nba
      * @return \think\response\Json
      * 球队详情
      */
-    public function team_info(){
+    public function team_info()
+    {
         $id = (isset($_GET['teamId'])) ? $_GET ['teamId'] : "24";
         $res = HttpGet("https://live.3g.qq.com/g/s?aid=action_api&module=nba&action=team_detail&teamId={$id}&sid=");
         return json([
@@ -118,7 +124,8 @@ class Nba
     /**
      * 球队阵容
      */
-    public function Lineup(){
+    public function Lineup()
+    {
         $id = (isset($_GET['teamId'])) ? $_GET ['teamId'] : "24";
         $res = HttpGet("https://live.3g.qq.com/g/s?aid=action_api&module=nba&action=team_player&teamId={$id}&sid=");
         return json([
@@ -133,17 +140,18 @@ class Nba
      * @return \think\response\Json
      * 网易NBA新闻列表
      */
-    public function new_list(){
-        $page  = (isset($_GET['page'])) ? $_GET ['page'] : "0";
-        $page= $page *15;
+    public function new_list()
+    {
+        $page = (isset($_GET['page'])) ? $_GET ['page'] : "0";
+        $page = $page * 15;
         $res = HttpGet("https://3g.163.com/touch/reconstruct/article/list/BD2AQH4Qwangning/{$page}-15.html");
-		$arr=json_decode(substr($res, 9, -1), true)['BD2AQH4Qwangning'];
-		//数据里面有一些直播的新闻数据、需要删除那些数据
-		foreach ($arr as $k=>$v) {
-			if(!empty($arr[$k]['liveInfo'])){
-				unset($arr[$k]);  
-			}	 
-		}
+        $arr = json_decode(substr($res, 9, -1), true)['BD2AQH4Qwangning'];
+        //数据里面有一些直播的新闻数据、需要删除那些数据
+        foreach ($arr as $k => $v) {
+            if (!empty($arr[$k]['liveInfo'])) {
+                unset($arr[$k]);
+            }
+        }
         return json([
             'msg' => 'success',
             'code' => 1,
@@ -155,7 +163,8 @@ class Nba
      * @return \think\response\Json
      * 网易NBA新闻详情
      */
-    public  function news_info(){
+    public function news_info()
+    {
         $id = (isset($_GET['docid'])) ? $_GET ['docid'] : "D22DCS5S0005877U";
         $res = HttpGet("http://3g.163.com/touch/article/{$id}/full.html");
         $arr = json_decode(substr($res, 12, -1), true);
@@ -170,12 +179,13 @@ class Nba
      * @return \think\response\Json
      * 文章评论
      */
-    public function news_comments(){
+    public function news_comments()
+    {
         $id = (isset($_GET['docid'])) ? $_GET ['docid'] : "D22DCS5S0005877U";
-        $res=HttpGet("https://comment.news.163.com/api/v1/products/a2869674571f77b5a0867c3d71db5856/threads/{$id}/comments/newList?offset=0&limit=10&headLimit=1&tailLimit=2&ibc=newswap&showLevelThreshold");
+        $res = HttpGet("https://comment.news.163.com/api/v1/products/a2869674571f77b5a0867c3d71db5856/threads/{$id}/comments/newList?offset=0&limit=10&headLimit=1&tailLimit=2&ibc=newswap&showLevelThreshold");
         $arr = json_decode($res, true)['comments'];
         $newArr = array();
-        foreach($arr as $k=>$v){
+        foreach ($arr as $k => $v) {
             array_push($newArr, $arr[$k]);
         }
         return json([
@@ -186,26 +196,31 @@ class Nba
     }
 
 
-    public function website(){
-          return json([
+    /**
+     * @return \think\response\Json
+     * 关于个人的JSON数据
+     */
+    public function website()
+    {
+        return json([
             'msg' => 'success',
             'code' => 1,
             'data' => [
-                'name'=>"没有故事的小明同学",
-                'job'=>"Web开发工程师",
-                'icon'=>"https://coding.it919.cn/static/images/zixia.jpg",
-                'address'=>"深圳市南山区科技园",
-                'latitude'=>"22.549990",
-                'longitude'=>"113.950660",
-                'github'=>"https://github.com/ecitlm",
-                'blog'=>"https://code.it919.cn",
-                'mail'=>"ecitlm@163.com",
-                'Motto'=>'我们这一生，要走很远的路，有如水坦途，有荆棘挡道；有簇拥同行，有孤独漫步；有幸福如影，有苦痛袭扰；有迅跑，有疾走，有徘徊，还有回首……正因为走了许多路，经历的无数繁华与苍凉，才在时光的流逝中体会岁月的变迁，让曾经稚嫩的心慢慢地趋于成熟。',
-                'music'=>[
-                    'src'=>"https://coding.it919.cn/static/images/better_man.mp3",
-                    'author'=>"Robbie Williams",
-                    'name'=>"Better Man",
-                    'poster'=>"https://coding.it919.cn/static/images/poster.jpg"
+                'name' => "没有故事的小明同学",
+                'job' => "Web开发工程师",
+                'icon' => "https://coding.it919.cn/static/images/zixia.jpg",
+                'address' => "深圳市南山区科技园",
+                'latitude' => "22.549990",
+                'longitude' => "113.950660",
+                'github' => "https://github.com/ecitlm",
+                'blog' => "https://code.it919.cn",
+                'mail' => "ecitlm@163.com",
+                'Motto' => '我们这一生，要走很远的路，有如水坦途，有荆棘挡道；有簇拥同行，有孤独漫步；有幸福如影，有苦痛袭扰；有迅跑，有疾走，有徘徊，还有回首……正因为走了许多路，经历的无数繁华与苍凉，才在时光的流逝中体会岁月的变迁，让曾经稚嫩的心慢慢地趋于成熟。',
+                'music' => [
+                    'src' => "https://coding.it919.cn/static/images/better_man.mp3",
+                    'author' => "Robbie Williams",
+                    'name' => "Better Man",
+                    'poster' => "https://coding.it919.cn/static/images/poster.jpg"
                 ]
             ]
         ]);
