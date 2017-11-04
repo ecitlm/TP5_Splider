7cdb19aa9d878410b0e8bfcf6eb6ab82af2bc6c7
ecitlm <ecitlm@163.com>
2017-11-04 10:11:58 +0800

    add

--- index.php
+++ index.php
@@ -1,35 +1,6 @@
 <?php
 
 
-
-
-//获取域名或主机地址
-echo $_SERVER['HTTP_HOST']."<br />";
- 
-//获取网页地址
-echo $_SERVER['PHP_SELF']."<br />";
- 
-//获取网址参数
-echo $_SERVER["QUERY_STRING"]."<br />";
- 
-//获取用户代理
-echo $_SERVER['HTTP_REFERER']."<br />";
- 
-//获取完整的url
-echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
- 
-//包含端口号的完整url
-echo 'http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
- 
-//只取路径
-$url='http://'.$_SERVER['SERVER_NAME'].$_SERVER["REQUEST_URI"];
-echo dirname($url);
-
-die();
-
-
-
 header("Access-Control-Allow-Origin: *");
 //入口文件绑定模块名
 //define('BIND_MODULE','api');
